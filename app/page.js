63209788b32ed536f73e9aaf8315b947af014426
Markus Luis Flores <markuslsflores@gmail.com>
2024-10-07 17:34:24 -0600

    Week 5 Assignment changes

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
           <li><Link href="./week-2" className="underline text-cyan-600 hover:text-cyan-300">Week 2 Assignment</Link></li>
           <li><Link href="./week-3" className="underline text-cyan-600 hover:text-cyan-300">Week 3 Assignment</Link></li>
           <li><Link href="./week-4" className="underline text-cyan-600 hover:text-cyan-300">Week 4 Assignment</Link></li>
+          <li><Link href="./week-5" className="underline text-cyan-600 hover:text-cyan-300">Week 5 Assignment</Link></li>
         </ul>
       </div>
     </main>
